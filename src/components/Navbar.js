67992e0e10390a954fb0849f4b505028d9d4da18
Nathalie Rautell <nathalierautell@Nathalies-MacBook-Pro.local>
2020-03-24 03:39:08 +0100

    added input fields

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,11 +4,24 @@ export const Navbar = () => {
   return (
     <>
       <nav>
-        <ul>
-          <li>Filter by price</li>
-          <li>Sort by price</li>
-          <li>Choose delivery option</li>
-        </ul>
+        <form action="">
+          <select>
+            <option value="" selected>Sort by price</option>
+            <option value="ascending">Ascending (low to high)</option>
+            <option value="descending">Descending (high to low)</option>
+          </select>
+        </form>
+        <form action="">
+          <select>
+            <option value="" selected>Sort by rating</option>
+            <option value="ascending">Ascending (low to high)</option>
+            <option value="descending">Descending (high to low)</option>
+          </select>
+        </form>
+        <form action="">
+          <label htmlFor="home">Home delivery<input type="checkbox" name="delivery" id="home" value="home" /></label>
+          <label htmlFor="table">Table booking<input type="checkbox" name="delivery" id="table" value="table" /></label>
+        </form>
       </nav>
     </>
   )
